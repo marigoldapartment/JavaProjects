ea95c63e3d1c230e6fd5990aa5186df3a121dda1
marigoldapartment <marigoldapartment1229@gmail.com>
2020-12-30 19:00:49 +0530

    shashank:javalearning:leap year

---
+++ PrimeNumber/src/Factorialclass.java
@@ -0,0 +1,10 @@
+public class Factorialclass {
+    public static void main(String[] args) {
+        int meranum = 5;
+        int factorial = 1;
+        for (int i = 1; i <= 5; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println("factorial will be " + factorial);
+    }
+}
