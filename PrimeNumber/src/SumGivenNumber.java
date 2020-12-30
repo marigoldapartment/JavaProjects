70dee3679b22bc59bde71e486e8fb1639a44e536
marigoldapartment <marigoldapartment1229@gmail.com>
2020-12-30 17:42:21 +0530

    Shashank:Learning java:Modified Primenumber and added Sum of given number code

---
+++ PrimeNumber/src/SumGivenNumber.java
@@ -0,0 +1,12 @@
+public class SumGivenNumber {
+    public static void main(String[] args) {
+        int meraNumber = 10;
+        int sum =0;
+        for(int i =0; i<=meraNumber;i++){
+            sum = sum + i;
+        }
+
+        System.out.println("Sum of the given number is : "+sum);
+
+    }
+}
