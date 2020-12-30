800dbb072bafc8f0f9790c725a857512f3a0fb72
marigoldapartment <marigoldapartment1229@gmail.com>
2020-12-30 20:33:15 +0530

    Subhash:Assignment 1: SliptAndMultiple

---
+++ PrimeNumber/src/SliptAndMultiple.java
@@ -0,0 +1,9 @@
+public class SliptAndMultiple {
+    public static void main(String[] args) {
+        System.out.println(splitAndMultiple(2990));
+    }
+
+    public static int splitAndMultiple(int meraNumber){
+
+    }
+}
