f7fbdd6b3603b41bd32ebb9c27649a33d3ccf64d
marigoldapartment <marigoldapartment1229@gmail.com>
2020-12-31 14:39:06 +0530

    rahul:assiement1completed:learningjava

--- PrimeNumber/src/SliptAndMultiple.java
+++ PrimeNumber/src/SliptAndMultiple.java
@@ -1,9 +1,33 @@
+import java.awt.color.ICC_Profile;
+import java.awt.im.InputMethodHighlight;
+
 public class SliptAndMultiple {
     public static void main(String[] args) {
-        System.out.println(splitAndMultiple(2990));
+        System.out.println(splitAndMultiple(2991));
     }
 
     public static int splitAndMultiple(int meraNumber){
+        int A=0;
+        int B=0;
+        int C=0;
+        int D=0;
+        int E=0;
+        int F=0;
+        int G=0;
+        D=meraNumber%10;
+        A=meraNumber/10;
+        E=A%10;
+        B=A/10;
+        F=B%10;
+        C=B/10;
+        G=C*F*E*D;
+
+        /*System.out.println("spliting the numbers  "+C+"  "+F+"  "+E+"  "+D );
+        System.out.println("your factorial of your number  "+G);*/
+
+        System.out.println("Product of your number" + " is "+ G);
+        return G;
+
 
     }
 }
