7bfb82c944a5d881ddbeb66b024bbeef5fe39ac7
marigoldapartment <marigoldapartment1229@gmail.com>
2021-01-07 14:28:33 +0530

    Subhash: Learning Java: Print Star pattern 2

---
+++ PrimeNumber/src/patternstar/StarPattern2.java
@@ -0,0 +1,16 @@
+package patternstar;
+
+public class StarPattern2 {
+    public static void main(String[] args) {
+        for(int i = 0; i<=5 ;i++){
+            for(int j = (4-i); j>=0;j--){
+                System.out.print(" ");
+            }
+            for(int k=0; k<=i;k++){
+                System.out.print(" *");
+            }
+
+            System.out.println();
+        }
+    }
+}
